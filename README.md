2a72f1394f0261d460e0a198528c0f70debf72da
frannyfra <francescadelaurentis1987@gmail.com>
2020-04-28 19:03:24 +0100

    deleted repeating files

--- README.md
+++
@@ -1,2 +0,0 @@
-
-A personal repo created in order to store mini-projects, tech-tests practice and katas solutions, with a fancy name suggested by Github.
--- src/static/images.js
+++
@@ -1,6 +0,0 @@
-const images = {
-    rome: "https://www.fodors.com/wp-content/uploads/2018/10/HERO_UltimateRome_Hero_shutterstock789412159.jpg"
-}
-
-
-export default images;
